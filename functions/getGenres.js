fb7dbca4b2cfb4955e002db8192b275913a55644
Rafal Porzucek <rafal.porzucek02@dixonscarphone.com>
2021-06-01 18:56:47 +0100

    Update to get limit

--- functions/getGenres.js
+++ functions/getGenres.js
@@ -1,12 +1,14 @@
 const fetch = require('node-fetch');
 
-exports.handler = async function () {
+exports.handler = async function (event) {
+  const limit = JSON.parse(event.body);
   const url = process.env.ASTRA_GRAPHQL_ENDPOINT;
   const token = process.env.ASTRA_DB_TOKEN;
   const query = `
     query getAllGenres {
       reference_list (
-        value: { label: "genre" }
+        value: { label: "genre" },
+        options: { limit: ${JSON.stringify(limit)} }
       ) {
         values {
           value
@@ -32,8 +34,6 @@ exports.handler = async function () {
       body: JSON.stringify(responseBody)
     }
   } catch (error) {
-    console.log(error);
-
     return {
       statusCode: 500,
       body: JSON.stringify(error)
