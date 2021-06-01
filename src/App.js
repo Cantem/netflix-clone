dcb847e8e6a381ad771b96d86236456184b03fef
Rafal Porzucek <rafal.porzucek02@dixonscarphone.com>
2021-06-01 18:57:20 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import { useEffect, useState } from "react";
 import Section from './components/Section';
+import Navbar from './components/Navbar';
+import HeroSection from './components/HeroSection';
 import './App.css';
 
 function App() {
+  const genresCount = 4;
   const [ genres, setGenres ] = useState(null);
+  const [limit, setLimit] = useState(genresCount);
 
   const fetchData = async () => {
-    const response = await fetch('/.netlify/functions/getGenres');
+    const response = await fetch('/.netlify/functions/getGenres', {
+      method: "POST",
+      body: limit
+    });
     const responseBody = await response.json();
     setGenres(responseBody.data.reference_list.values);
   }
 
   useEffect(() => {
     fetchData();
-  }, []);
-
-  // const uniqueKey = (index) => `${Math.floor(Math.random() * 100)}_${index}`;
+  }, [, limit]);
 
   return (
     <>
-     {genres && Object.values(genres).map((genre, index) => (<Section genre={genre.value}/>))}
+    <Navbar />
+    <HeroSection />
+    <div className='container'>
+      {genres && Object.values(genres).map((genre, index) => (<Section key={index} genre={genre.value}/>))}
+    </div>
+     <div className='page-end'
+      onMouseEnter={ () => {
+        setLimit(limit + genresCount);
+      }}
+     />
     </>
   );
 }
